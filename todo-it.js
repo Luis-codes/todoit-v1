135c16b1e92f8de95ac4d6d92c59000b1d8b39df
Luis rosado <luismrosado@yahoo.com>
2020-01-11 11:30:37 -0500

    todo adding list items

--- todo-it.js
+++ todo-it.js
@@ -1,1 +1,22 @@
 console.log("ToDoIt");
+var todoList = [];
+console.log("Current todo list: ", todoList);
+var todoInput = document.getElementById('todoInput');
+function addTodo() {
+    // if we don't have the todo input
+    if (todoInput == null) {
+        console.error('The todo input is missing from the page!');
+        return;
+    }
+    // get the value from the input
+    var newTodo = todoInput.value;
+    // verify that there is text
+    if ('' !== newTodo.trim()) {
+        console.log('Adding todo: ', newTodo);
+        // add the new item to the list
+        todoList.push(newTodo);
+        console.log('New todo list: ', todoList);
+        // clear the input
+        todoInput.value = '';
+    }
+}
--- todo-it.ts
+++ todo-it.ts
@@ -1,1 +1,30 @@
 console.log("ToDoIt");
+
+const todoList: string[] = [];
+
+console.log("Current todo list: ", todoList);
+
+const todoInput: HTMLInputElement = document.getElementById('todoInput') as HTMLInputElement;
+
+function addTodo() : void {
+    // if we don't have the todo input
+    if(todoInput == null) {
+        console.error('The todo input is missing from the page!');
+        return;
+    }
+
+    // get the value from the input
+    const newTodo: string = todoInput.value;
+
+    // verify that there is text
+    if ('' !== newTodo.trim()) {
+        console.log('Adding todo: ', newTodo);
+
+        // add the new item to the list
+        todoList.push(newTodo);
+        console.log('New todo list: ', todoList);
+
+        // clear the input
+        todoInput.value = '';
+    }
+}
