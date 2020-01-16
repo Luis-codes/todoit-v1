475f5ff96755b0f11b8a3264d504381e3589e2e7
Luis rosado <luismrosado@yahoo.com>
2020-01-15 23:11:40 -0500

    choosing between interfaces || classes

--- todo-it.js
+++ todo-it.js
@@ -1,16 +1,3 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 console.log("ToDoIt");
 var todoList = [];
 console.log("Current todo list: ", todoList);
@@ -90,162 +77,171 @@ function removeTodoListItem(itemToRemove) {
     // apply the todo list filter
     filterTodoList();
 }
-var Car = /** @class */ (function () {
-    function Car() {
-    }
-    return Car;
-}());
-var car = new Car();
-var ColoredCar = /** @class */ (function () {
-    function ColoredCar(color) {
-        this._color = color;
-    }
-    ColoredCar.prototype.displayColor = function () {
-        console.log("Color of this car: " + this._color);
-    };
-    Object.defineProperty(ColoredCar.prototype, "color", {
-        get: function () {
-            return this._color;
-        },
-        set: function (color) {
-            this._color = color;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    ColoredCar.prototype.resetColor = function () {
-        this._color = ColoredCar.DEFAULT_COLOR;
-    };
-    ColoredCar.DEFAULT_COLOR = "Red";
-    return ColoredCar;
-}());
-var Shape = /** @class */ (function () {
-    function Shape(_shapeName) {
-        this._shapeName = _shapeName;
-        this.displayInformation();
-    }
-    Object.defineProperty(Shape.prototype, "shapeName", {
-        get: function () {
-            return this._shapeName;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Shape.prototype.displayInformation = function () {
-        console.log("This shape is a " + this._shapeName);
-    };
-    Shape.prototype.doSomething = function () {
-        console.log("Not Interesting");
-    };
-    return Shape;
-}());
-var Square = /** @class */ (function (_super) {
-    __extends(Square, _super);
-    function Square(_width) {
-        var _this = _super.call(this, "Square") || this;
-        _this._width = _width;
-        return _this;
-    }
-    Square.prototype.displayArea = function () {
-        var area = this._width * this._width;
-        console.log("This " + this.shapeName + " has an area of: " + area);
-    };
-    Square.prototype.displayPerimeter = function () {
-        var perimeter = 2 * (this._width + this._width);
-        console.log("This " + this.shapeName + " has a perimeter of: " + perimeter);
-    };
-    Square.prototype.doSomething = function () {
-        console.log("Something more interesting");
-    };
-    return Square;
-}(Shape));
-var square = new Square(5);
-square.displayArea();
-square.displayPerimeter();
-square.doSomething();
-var Calculator = /** @class */ (function () {
-    function Calculator(_currentValue) {
-        if (_currentValue === void 0) { _currentValue = 0; }
-        this._currentValue = _currentValue;
-    }
-    Calculator.prototype.add = function (a) {
-        this._currentValue += a;
-        return this;
-    };
-    Calculator.prototype.subtract = function (a) {
-        this._currentValue -= a;
-        return this;
-    };
-    Calculator.prototype.multiply = function (a) {
-        this._currentValue *= a;
-        return this;
-    };
-    Calculator.prototype.divide = function (a) {
-        this._currentValue /= a;
-        return this;
-    };
-    Object.defineProperty(Calculator.prototype, "value", {
-        get: function () {
-            return this._currentValue;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return Calculator;
-}());
-var result = new Calculator(0)
-    .add(5)
-    .multiply(2)
-    .add(10)
-    .divide(4)
-    .subtract(2)
-    .value;
-console.log("Result: " + result);
-var plane = {
-    name: "Plane",
-    description: "Something that flies"
-};
-function foo(bar) {
-    console.log("Hello " + bar.firstName + ".. or should I call you Mr." + bar.lastName + "?");
-}
-foo({
-    firstName: "Sebastien",
-    lastName: "Dubois"
-});
-function performCalculation(a, b, calculationFn) {
-    console.log("The result is " + calculationFn(a, b));
-}
-performCalculation(5, 10, function (x, y) { return x + y; });
-var myThing = {
-    name: "Computer",
-    description: "A thing that can perform calculation"
-};
-var BasicMusicPlayer = /** @class */ (function () {
-    function BasicMusicPlayer() {
-    }
-    BasicMusicPlayer.prototype.fastForward = function (seconds) {
-        console.log("Moving forward " + seconds + " seconds");
-    };
-    BasicMusicPlayer.prototype.pause = function () {
-        console.log("Pausing");
-    };
-    BasicMusicPlayer.prototype.play = function () {
-        console.log("Playing");
-    };
-    BasicMusicPlayer.prototype.rewind = function (seconds) {
-        console.log("Rewinding " + seconds);
-    };
-    BasicMusicPlayer.prototype.stop = function () {
-        console.log("Stopping");
-    };
-    return BasicMusicPlayer;
-}());
-function sayHelloTo(bar) {
-    console.log("Hello " + bar.firstName + ".. or should i call you Mr." + bar.lastName + "?");
-}
-var johnDoe = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 42
-};
-sayHelloTo(johnDoe);
+// class Car {
+// }
+// let car: Car = new Car();
+// class ColoredCar {
+//     private _color: string;
+//     private static DEFAULT_COLOR = "Red";
+//     constructor(color: string) {
+//         this._color = color;
+//     }
+//     displayColor() {
+//         console.log(`Color of this car: ${this._color}`);
+//     }
+//     public get color(): string {
+//         return this._color;
+//     }
+//     public set color(color: string) {
+//         this._color = color
+//     }
+//     private resetColor() {
+//         this._color = ColoredCar.DEFAULT_COLOR;
+//     }
+// }
+// abstract class Shape {
+//     constructor(private readonly _shapeName: string) {
+//         this.displayInformation();
+//     }
+//     abstract displayArea(): void;
+//     abstract displayPerimeter(): void;
+//     protected get shapeName(): string {
+//         return this._shapeName
+//     }
+//     public displayInformation(): void {
+//         console.log(`This shape is a ${this._shapeName}`);
+//     }
+//     public doSomething(): void {
+//         console.log("Not Interesting");
+//     }
+// }
+// class Square extends Shape {
+//     constructor(private _width: number) {
+//         super("Square")
+//     }
+//     displayArea(): void {
+//         const area = this._width * this._width;
+//         console.log(`This ${this.shapeName} has an area of: ${area}`);
+//     }
+//     displayPerimeter(): void {
+//         const perimeter = 2 * (this._width + this._width);
+//         console.log(`This ${this.shapeName} has a perimeter of: ${perimeter}`);
+//     }
+//     public doSomething(): void {
+//         console.log("Something more interesting");
+//     }
+// }
+// let square: Square = new Square(5);
+// square.displayArea();
+// square.displayPerimeter();
+// square.doSomething();
+// class Calculator {
+//     constructor(private _currentValue: number = 0) { }
+//     add(a: number): this {
+//         this._currentValue += a;
+//         return this;
+//     }
+//     subtract(a: number): this {
+//         this._currentValue -= a;
+//         return this;
+//     }
+//     multiply(a: number): this {
+//         this._currentValue *= a;
+//         return this;
+//     }
+//     divide(a: number): this {
+//         this._currentValue /= a;
+//         return this;
+//     }
+//     get value(): number {
+//         return this._currentValue;
+//     }
+// }
+// let result: number = new Calculator(0)
+// .add(5) 
+// .multiply(2)
+// .add(10)
+// .divide(4)
+// .subtract(2)
+// .value;
+// console.log(`Result: ${result}`);
+// const plane: {
+//     name: string,
+//     description: string
+// } = {
+//     name:"Plane",
+//     description: "Something that flies"
+// };
+// function foo(bar: { firstName: string, lastName: string }): void {
+//     console.log(`Hello ${bar.firstName}.. or should I call you Mr.${bar.lastName}?`);
+// }
+// foo({
+//     firstName: "Sebastien",
+//     lastName: "Dubois"
+// });
+// function performCalculation(
+//     a: number,
+//     b: number,
+//     calculationFn: (x: number, y: number) => number
+// ): void {
+//     console.log(`The result is ${calculationFn(a, b) }`);
+// }
+// performCalculation(
+//     5,
+//     10,
+//     (x: number, y: number) => x + y
+// );
+// type Thing = {
+//     name: string,
+//     description: string
+// };
+// const myThing: Thing = {
+//     name: "Computer",
+//     description: "A thing that can perform calculation"
+// };
+// interface MusicPlayer {
+//     play(): void;
+//     pause(): void;
+//     stop(): void;
+//     rewind(seconds: number): void;
+//     fastForward(seconds: number): void;
+// }
+// class BasicMusicPlayer implements MusicPlayer {
+//     fastForward(seconds: number): void {
+//         console.log(`Moving forward ${seconds} seconds`);
+//     }
+//     pause(): void {
+//         console.log("Pausing");
+//     }
+//     play(): void {
+//         console.log("Playing")
+//     }
+//     rewind(seconds: number): void {
+//         console.log(`Rewinding ${seconds}`);
+//     }
+//     stop(): void {
+//         console.log("Stopping");
+//     }
+// }
+// interface Person {
+//     firstName: string,
+//     lastName: string,
+//     age: number
+// }
+// function sayHelloTo(bar: Person): void {
+//     console.log(`Hello ${bar.firstName}.. or should I call you Mr.${bar.lastName}?`);
+// }
+// let johnDoe: Person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 42
+// };
+// sayHelloTo(johnDoe);
+// interface Club {
+//     name: string;
+//     logoLocation: string;
+//     isActive(): boolean;
+// }
+// interface SoccerClub extends Club {
+//     league: string;
+// }
--- todo-it.ts
+++ todo-it.ts
@@ -103,204 +103,215 @@ function removeTodoListItem(itemToRemove: string): void {
     filterTodoList();
 }
 
-class Car {
+// class Car {
 
-}
+// }
 
-let car: Car = new Car();
+// let car: Car = new Car();
 
-class ColoredCar {
-    private _color: string;
-    private static DEFAULT_COLOR = "Red";
+// class ColoredCar {
+//     private _color: string;
+//     private static DEFAULT_COLOR = "Red";
 
-    constructor(color: string) {
-        this._color = color;
-    }
+//     constructor(color: string) {
+//         this._color = color;
+//     }
 
-    displayColor() {
-        console.log(`Color of this car: ${this._color}`);
-    }
+//     displayColor() {
+//         console.log(`Color of this car: ${this._color}`);
+//     }
 
-    public get color(): string {
-        return this._color;
-    }
+//     public get color(): string {
+//         return this._color;
+//     }
 
-    public set color(color: string) {
-        this._color = color
-    }
+//     public set color(color: string) {
+//         this._color = color
+//     }
 
-    private resetColor() {
-        this._color = ColoredCar.DEFAULT_COLOR;
-    }
-}
+//     private resetColor() {
+//         this._color = ColoredCar.DEFAULT_COLOR;
+//     }
+// }
 
-abstract class Shape {
-    constructor(private readonly _shapeName: string) {
-        this.displayInformation();
-    }
+// abstract class Shape {
+//     constructor(private readonly _shapeName: string) {
+//         this.displayInformation();
+//     }
 
-    abstract displayArea(): void;
-    abstract displayPerimeter(): void;
+//     abstract displayArea(): void;
+//     abstract displayPerimeter(): void;
 
-    protected get shapeName(): string {
-        return this._shapeName
-    }
+//     protected get shapeName(): string {
+//         return this._shapeName
+//     }
 
-    public displayInformation(): void {
-        console.log(`This shape is a ${this._shapeName}`);
-    }
+//     public displayInformation(): void {
+//         console.log(`This shape is a ${this._shapeName}`);
+//     }
 
-    public doSomething(): void {
-        console.log("Not Interesting");
-    }
-}
+//     public doSomething(): void {
+//         console.log("Not Interesting");
+//     }
+// }
 
-class Square extends Shape {
-    constructor(private _width: number) {
-        super("Square")
-    }
+// class Square extends Shape {
+//     constructor(private _width: number) {
+//         super("Square")
+//     }
 
-    displayArea(): void {
-        const area = this._width * this._width;
-        console.log(`This ${this.shapeName} has an area of: ${area}`);
-    }
+//     displayArea(): void {
+//         const area = this._width * this._width;
+//         console.log(`This ${this.shapeName} has an area of: ${area}`);
+//     }
 
-    displayPerimeter(): void {
-        const perimeter = 2 * (this._width + this._width);
-        console.log(`This ${this.shapeName} has a perimeter of: ${perimeter}`);
-    }
+//     displayPerimeter(): void {
+//         const perimeter = 2 * (this._width + this._width);
+//         console.log(`This ${this.shapeName} has a perimeter of: ${perimeter}`);
+//     }
 
-    public doSomething(): void {
-        console.log("Something more interesting");
-    }
-}
+//     public doSomething(): void {
+//         console.log("Something more interesting");
+//     }
+// }
 
-let square: Square = new Square(5);
-square.displayArea();
-square.displayPerimeter();
-square.doSomething();
+// let square: Square = new Square(5);
+// square.displayArea();
+// square.displayPerimeter();
+// square.doSomething();
 
-class Calculator {
-    constructor(private _currentValue: number = 0) { }
+// class Calculator {
+//     constructor(private _currentValue: number = 0) { }
 
-    add(a: number): this {
-        this._currentValue += a;
-        return this;
-    }
+//     add(a: number): this {
+//         this._currentValue += a;
+//         return this;
+//     }
 
-    subtract(a: number): this {
-        this._currentValue -= a;
-        return this;
-    }
+//     subtract(a: number): this {
+//         this._currentValue -= a;
+//         return this;
+//     }
 
-    multiply(a: number): this {
-        this._currentValue *= a;
-        return this;
-    }
+//     multiply(a: number): this {
+//         this._currentValue *= a;
+//         return this;
+//     }
 
-    divide(a: number): this {
-        this._currentValue /= a;
-        return this;
-    }
-    get value(): number {
-        return this._currentValue;
-    }
-}
+//     divide(a: number): this {
+//         this._currentValue /= a;
+//         return this;
+//     }
+//     get value(): number {
+//         return this._currentValue;
+//     }
+// }
 
-let result: number = new Calculator(0)
-.add(5) 
-.multiply(2)
-.add(10)
-.divide(4)
-.subtract(2)
-.value;
+// let result: number = new Calculator(0)
+// .add(5) 
+// .multiply(2)
+// .add(10)
+// .divide(4)
+// .subtract(2)
+// .value;
 
-console.log(`Result: ${result}`);
+// console.log(`Result: ${result}`);
 
-const plane: {
-    name: string,
-    description: string
-} = {
-    name:"Plane",
-    description: "Something that flies"
-};
+// const plane: {
+//     name: string,
+//     description: string
+// } = {
+//     name:"Plane",
+//     description: "Something that flies"
+// };
 
-function foo(bar: { firstName: string, lastName: string }): void {
-    console.log(`Hello ${bar.firstName}.. or should I call you Mr.${bar.lastName}?`);
-}
+// function foo(bar: { firstName: string, lastName: string }): void {
+//     console.log(`Hello ${bar.firstName}.. or should I call you Mr.${bar.lastName}?`);
+// }
 
-foo({
-    firstName: "Sebastien",
-    lastName: "Dubois"
-});
+// foo({
+//     firstName: "Sebastien",
+//     lastName: "Dubois"
+// });
 
-function performCalculation(
-    a: number,
-    b: number,
-    calculationFn: (x: number, y: number) => number
-): void {
-    console.log(`The result is ${calculationFn(a, b) }`);
-}
-performCalculation(
-    5,
-    10,
-    (x: number, y: number) => x + y
-);
+// function performCalculation(
+//     a: number,
+//     b: number,
+//     calculationFn: (x: number, y: number) => number
+// ): void {
+//     console.log(`The result is ${calculationFn(a, b) }`);
+// }
+// performCalculation(
+//     5,
+//     10,
+//     (x: number, y: number) => x + y
+// );
 
-type Thing = {
-    name: string,
-    description: string
-};
+// type Thing = {
+//     name: string,
+//     description: string
+// };
 
-const myThing: Thing = {
-    name: "Computer",
-    description: "A thing that can perform calculation"
-};
+// const myThing: Thing = {
+//     name: "Computer",
+//     description: "A thing that can perform calculation"
+// };
 
-interface MusicPlayer {
-    play(): void;
-    pause(): void;
-    stop(): void;
-    rewind(seconds: number): void;
-    fastForward(seconds: number): void;
-}
+// interface MusicPlayer {
+//     play(): void;
+//     pause(): void;
+//     stop(): void;
+//     rewind(seconds: number): void;
+//     fastForward(seconds: number): void;
+// }
 
-class BasicMusicPlayer implements MusicPlayer {
-    fastForward(seconds: number): void {
-        console.log(`Moving forward ${seconds} seconds`);
-    }
+// class BasicMusicPlayer implements MusicPlayer {
+//     fastForward(seconds: number): void {
+//         console.log(`Moving forward ${seconds} seconds`);
+//     }
 
-    pause(): void {
-        console.log("Pausing");
-    }
+//     pause(): void {
+//         console.log("Pausing");
+//     }
 
-    play(): void {
-        console.log("Playing")
-    }
+//     play(): void {
+//         console.log("Playing")
+//     }
 
-    rewind(seconds: number): void {
-        console.log(`Rewinding ${seconds}`);
-    }
+//     rewind(seconds: number): void {
+//         console.log(`Rewinding ${seconds}`);
+//     }
 
-    stop(): void {
-        console.log("Stopping");
-    }
-}
+//     stop(): void {
+//         console.log("Stopping");
+//     }
+// }
 
-interface Person {
-    firstName: string,
-    lastName: string,
-    age: number
-}
+// interface Person {
+//     firstName: string,
+//     lastName: string,
+//     age: number
+// }
 
-function sayHelloTo(bar: Person): void {
-    console.log(`Hello ${bar.firstName}.. or should i call you Mr.${bar.lastName}?`);
-}
+// function sayHelloTo(bar: Person): void {
+//     console.log(`Hello ${bar.firstName}.. or should I call you Mr.${bar.lastName}?`);
+// }
 
-let johnDoe: Person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 42
-};
+// let johnDoe: Person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 42
+// };
 
-sayHelloTo(johnDoe);
+// sayHelloTo(johnDoe);
+
+// interface Club {
+//     name: string;
+//     logoLocation: string;
+
+//     isActive(): boolean;
+// }
+
+// interface SoccerClub extends Club {
+//     league: string;
+// }
