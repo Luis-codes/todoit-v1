984a35da56dcd6995fa45302bd6e96f59cf5d6e7
Luis rosado <luismrosado@yahoo.com>
2020-01-15 22:19:02 -0500

    inline types

--- todo-it.ts
+++ todo-it.ts
@@ -216,3 +216,20 @@ let result: number = new Calculator(0)
 .value;
 
 console.log(`Result: ${result}`);
+
+const plane: {
+    name: string,
+    description: string
+} = {
+    name:"Plane",
+    description: "Something that flies"
+};
+
+function foo(bar: { firstName: string, lastName: string }): void {
+    console.log(`Hello ${bar.firstName}.. or should I call you Mr.${bar.lastName}?`);
+}
+
+foo({
+    firstName: "Sebastien",
+    lastName: "Dubois"
+});
